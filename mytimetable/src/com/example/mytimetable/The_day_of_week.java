ca02f3c4c862d2d34b7cc8eb8912b1f5b5c90524
PashaChu <113158505+PashaChu@users.noreply.github.com>
2022-11-05 11:01:52 +0500

    Add files via upload

---
+++ mytimetable/src/com/example/mytimetable/The_day_of_week.java
@@ -0,0 +1,13 @@
+package com.example.mytimetable;
+
+import java.io.*;
+import java.util.*;
+
+public class The_day_of_week {
+   // private ArrayList <> profList;
+    // private ArrayList
+
+    public The_day_of_week(){
+
+    }
+}
