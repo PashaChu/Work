4af53a4313bce3d75060214317a8b8138924246b
PashaChu <113158505+PashaChu@users.noreply.github.com>
2022-11-12 19:43:29 +0500

    Add files via upload

--- myTimeTable/src/com/example/myTimeTable/Week.java
+++ myTimeTable/src/com/example/myTimeTable/Week.java
@@ -1,8 +1,42 @@
 package com.example.myTimeTable;
 
-public class Week {
+import java.util.Scanner;
 
-    public Week(){
+public class Week extends Professor{
+    private Professor listProf;
+    private String nameG;
+    private String room;
 
+    private String objectW;
+
+    public Week() {
+        this.listProf = new Professor();
+        this.nameG = new String();
+        this.room = new String();
+        this.objectW = new String();
+    }
+
+    public void copy(Professor a){
+        this.listProf = a;
+    }
+
+    public void writeGroup(String g){
+        this.nameG = g;
+    }
+
+    public void writeRoom(String r){
+        this.room = r;
+    }
+
+    public void writeObj(String obj){
+        this.objectW = obj;
+    }
+
+    public void printWeek(){
+        String a = listProf.setName();
+        System.out.println("Преподаватель:" + a);
+        System.out.println("Предмет:" + objectW);
+        System.out.println("Группа:" + nameG);
+        System.out.println("Аудитория:" + room);
     }
 }
