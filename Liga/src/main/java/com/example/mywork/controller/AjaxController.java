6876d228cd827474ff8507298853d5411e80d812
PashaChu <113158505+PashaChu@users.noreply.github.com>
2022-12-30 15:30:31 +0500

    Add files via upload

--- Liga/src/main/java/com/example/mywork/controller/AjaxController.java
+++ Liga/src/main/java/com/example/mywork/controller/AjaxController.java
@@ -1,34 +1,33 @@
 package com.example.mywork.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
 import java.io.*;
+import java.util.Scanner;
 
 @RestController
 public class AjaxController {
     @RequestMapping(value = "/like", method = RequestMethod.POST)
-    public void list(@RequestParam(value = "Username") String Username) {
+    @ResponseStatus
+    public ResponseEntity<?> list(@RequestBody String Username) {
         try {
             File file = new File("myList.txt");
             FileReader fr = new FileReader(file);
-            BufferedReader reader = new BufferedReader(fr);
-            String line = reader.readLine();
-            while (line != null) {
-                line = reader.readLine();
-                if(line.equals(Username)){
-                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            Scanner scanner = new Scanner(fr);
+            String line;
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                if (line.equalsIgnoreCase(Username)) {
+                    return new ResponseEntity<>("", HttpStatus.CONFLICT);
                 }
-            }
-            if(line == null){
-                throw new ResponseStatusException(HttpStatus.OK);
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (Exception e) { }
-        throw new ResponseStatusException(HttpStatus.OK);
+        }
+        return new ResponseEntity<>("", HttpStatus.OK);
     }
 }
+
 
