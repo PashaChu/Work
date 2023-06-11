df4f9623974a1d3cf7e015436cc94e585641d10b
PashaChu <113158505+PashaChu@users.noreply.github.com>
2023-06-11 21:01:09 +0500

    Add files via upload

--- Liga/src/test/java/com.example.mywork/ApplicationDemoTest.java
+++ Liga/src/test/java/com.example.mywork/ApplicationDemoTest.java
@@ -1,45 +1,37 @@
 package com.example.mywork;
 
 import com.example.mywork.controller.MyController;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
 import java.io.*;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class ApplicationDemoTest {
     @Mock
-    private Set<String> str;
-    public String Username;
+    private ObjectMapper objectMapper;
 
     @Test
     public void writeToFile(){
-        MyController s = mock(MyController.class);
-        Mockito.doNothing().when(s).player(Username);
-        s.player(Username);
-        verify(s, times(1)).player(Username);
-    }
-    @Test
-    public void testSave()throws Exception{
         try {
-            str = new HashSet<>();
-            str.add("Jeck");
-            Username = "Jeck";
-            Boolean control = MyController.save(str, "myList.txt", Username);
-            assertNotNull(control);
-        } catch (FileNotFoundException e){
-            fail("File not found");
+            Set<String>  str = new HashSet<>();
+            Path dataPath = Files.createTempFile("data", "json");
+            Mockito.doNothing().when(objectMapper).writeValue(any(File.class), any(MyController.class));
+            Mockito.verify(objectMapper, Mockito.times(0)).writeValue((dataPath).toFile(), MyController.save(str, "myList.txt", "TestName"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
-
-
-
 }
