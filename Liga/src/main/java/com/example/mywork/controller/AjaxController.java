68fa78063c672e0bc310d75d2ab158e1e96299bc
PashaChu <113158505+PashaChu@users.noreply.github.com>
2023-06-15 23:00:33 +0500

    Add files via upload

--- Liga/src/main/java/com/example/mywork/controller/AjaxController.java
+++ Liga/src/main/java/com/example/mywork/controller/AjaxController.java
@@ -7,26 +7,30 @@ import java.io.*;
 import java.util.Scanner;
 
 @RestController
-@CrossOrigin(origins = "http://*:8081")
+@CrossOrigin(origins = "http://localhost:8080")
 public class AjaxController {
+
     @RequestMapping(value = "/like", method = RequestMethod.POST)
     @ResponseStatus
     public ResponseEntity<?> list(@RequestBody String Username) {
         try {
-            File file = new File("myList.txt");
+            File file = setFile();
             FileReader fr = new FileReader(file);
             try (Scanner scanner = new Scanner(fr)) {
                 String line;
                 while (scanner.hasNextLine()) {
                     line = scanner.nextLine();
                     if (line.equalsIgnoreCase(Username)) {
-                        return new ResponseEntity<>("", HttpStatus.CONFLICT);
+                        return new ResponseEntity<>(HttpStatus.CONFLICT);
                     }
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new ResponseEntity<>("", HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    public File setFile() {
+        return new File("myList.txt");
     }
 }
